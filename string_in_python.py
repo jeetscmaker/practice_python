eec2e7888af361230a77a19b86c74044e534c76c
Jitendra <jeetscmaker@gmail.com>
2023-01-03 18:18:29 +0530

    length of string function len()

--- string_in_python.py
+++ string_in_python.py
@@ -7,3 +7,6 @@ print(message2)
 message3 = """multiline strings in python start
 and end with tripe quotes."""
 print(message3)
+
+length_of_string = len(message3)
+print("length of 'multiline strings in python start and end with tripe quotes.' is = " + str(length_of_string))
