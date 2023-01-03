423bfd3718cd21adf3415f2148724a3fa75bcbdf
Jitendra <jeetscmaker@gmail.com>
2023-01-03 18:31:35 +0530

    index of characters in string and slicing

--- string_in_python.py
+++ string_in_python.py
@@ -10,3 +10,14 @@ print(message3)
 
 length_of_string = len(message3)
 print("length of 'multiline strings in python start and end with tripe quotes.' is = " + str(length_of_string))
+
+# print the first character of message2
+print("The first char of message2 is = " + message2[0])
+
+# print the first 5 characters (from index 0 to index 4) of message2
+print("First 5 characters of message2: " + message2[0:5])
+# same output even if we remove 0 i the range because by default it counts from 0th index.
+print("First 5 characters of message2: " + message2[:5])
+
+# start from the 6th index and print till the end of message2
+print("Starting from 6th index till the end of message2: " + message2[6:])
