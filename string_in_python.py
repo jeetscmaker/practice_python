586d47099b3131397f9ced7ddbc0202740ba5433
Jitendra <jeetscmaker@gmail.com>
2023-01-03 18:12:32 +0530

    string in python

---
+++ string_in_python.py
@@ -0,0 +1,9 @@
+message = "Hello World."
+print(message)
+
+message2 = "Hello World with double quotes"
+print(message2)
+
+message3 = """multiline strings in python start
+and end with tripe quotes."""
+print(message3)
