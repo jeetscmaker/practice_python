abceeabf6141a67f47b8d0d023d284140a1df04e
Jitendra <jeetscmaker@gmail.com>
2023-01-10 15:55:57 +0530

    some more slicing examples

--- list_in_python.py
+++ list_in_python.py
@@ -28,3 +28,15 @@ print(more_squares)
 
 more_squares.append(64)
 print(more_squares)
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters)
+# replace some values
+letters[2:5] = ['C', 'D', 'E']
+print(letters)
+# now remove them
+letters[2:5] = []
+print(letters)
+# clear the list by replacing all the elements with an empty list
+letters[:] = []
+print(letters)
