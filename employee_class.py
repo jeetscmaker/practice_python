4827b310137cc0cfc85c4f6bc45d8c8c4a05e471
Jitendra <jeetscmaker@gmail.com>
2023-01-16 11:52:49 +0530

    explicitly passing argument to __init__ self

--- employee_class.py
+++ employee_class.py
@@ -6,13 +6,15 @@ class Employee(object):
 		self.sal = salary
 		self.email = first_name + '.' + last_name + '@company.com'
 
-	def full_name(self):
+	def full_name(self):      # This argument 'self' is very important
 		return '{} {}'.format(self.fname, self.lname)		
 
 emp_1 = Employee("John", "Dalton", 1000)
 emp_2 = Employee("Ray", "Palmer", 1500)
 
-print(emp_1.full_name())
+print(emp_1.full_name())         # emp1 is automatically passed as self.
 print(emp_2.full_name())
 print(emp_1.email)
 print(emp_2.email)
+print(Employee.full_name(emp_1)) # another way of passing argument to self.
+print(Employee.full_name(emp_2))
