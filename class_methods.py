afd2f9fc7a03eb5a9540aa93984b198dd39ea85b
Jitendra <jeetscmaker@gmail.com>
2023-01-17 22:25:38 +0530

    using class method as a constructor

--- class_methods.py
+++ class_methods.py
@@ -23,6 +23,11 @@ class Employee(object):
 	def set_raise_amount(cls, amount):
 		cls.raise_amount = amount
 
+	@classmethod
+	def from_string(cls, emp_str):
+		first_name, last_name, salary = emp_str.split('-')
+		return cls(first_name, last_name, salary)
+
 emp_1 = Employee("John", "Dalton", 1000)
 emp_2 = Employee("Ray", "Palmer", 1500)
 
@@ -35,4 +40,13 @@ emp_str_1 = "Don-Monhy-1000"
 first_name, last_name, salary = emp_str_1.split('-')
 emp_3 = Employee(first_name, last_name, salary)
 print(emp_3.full_name())
+
+''' creating an employee using from_string class method.
+ the class method must be called using the class name followed by dot operator 
+ followed by method name. '''
+emp_str_2 = "Jane-Alan-1500"
+emp_4 = Employee.from_string(emp_str_2)
+print(emp_4.full_name())
+
+
 
