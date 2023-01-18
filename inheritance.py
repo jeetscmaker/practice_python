7bdada29f00ecb317b64e801fe2644b825828977
Jitendra <jeetscmaker@gmail.com>
2023-01-18 12:46:46 +0530

    Manager inherits Employee

--- inheritance.py
+++ inheritance.py
@@ -1,7 +1,36 @@
 from class_methods import Employee
 
 class Developer(Employee):
-	pass
+	def __init__(self, first_name, last_name, salary, prog_lang):
+		super().__init__(first_name, last_name, salary)
+		# Employee.__init__(first_name, last_name, salary) # alternative to the super() call
+		self.programming_language = prog_lang
+
+class Manager(Employee):
+	def __init__(self, first_name, last_name, salary, employees = None):
+		super().__init__(first_name, last_name, salary)
+		if employees is None:
+			self.employees = []
+		else:
+			self.employees = employees
+
+	def add_emp(self, emp):
+		if emp not in self.employees:
+			employees.append(emp)
+	
+	def remove_emp(self, emp):
+		if emp in self.employees:
+			self.employees.remove(emp)		
+			
+	def print_emps(self):
+		for emp in self.employees:
+			print("-->", emp.full_name())
 
 dev_1 = Employee.from_string("Jane-Alan-1500")
 print(dev_1.full_name())
+
+dev_2 = Developer('Shawn', 'Herd', 1200, 'Java')
+print(dev_2.programming_language)
+
+manager_1 = Manager('Steve', 'Smith', 2000, [dev_1, dev_2])
+Manager.print_emps(manager_1)
