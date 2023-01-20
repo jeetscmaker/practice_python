ef8421de321ced9b2544a7ba7ab825333327c03c
Jitendra <jeetscmaker@gmail.com>
2023-01-20 13:03:10 +0530

    sets do not allow duplicates

--- tuples_in_python.py
+++ tuples_in_python.py
@@ -10,3 +10,24 @@ print(list_1, list_c)
 # Altering the list_c now to see that the changes are also reflected in list_1
 list_c[0] = 'Geology'
 print(list_1, list_c)
+
+# Tuple
+tuple_1 = ('History','Civics','Geography','Botany')
+tuple_c = tuple_1
+print(tuple_1, tuple_c)
+
+''' this will throw an error because tuple is immutable.
+ tuple_c[0] = 'Math' 
+ similarly we can not append or remove items from a tuple.
+ But we can access the element of a tuple. 
+'''
+print(tuple_c[0])
+
+'''
+	Sets in python are data structure which do not allow duplicates.
+	they are enclosed within curly brackets.
+	Also sets are unordered. That means items are not stored in certain order
+	therefore we can't access the items using index.
+'''
+set_1 = {'History','History', 'Geography'}
+print(set_1)  # prints {'History', 'Geography'}
