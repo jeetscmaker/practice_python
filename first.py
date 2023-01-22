8f3b74c986e464029324c8488e4f054353b3bb63
Jitendra <jeetscmaker@gmail.com>
2023-01-22 11:50:33 +0530

    f-string for concatenation

--- first.py
+++ first.py
@@ -2,3 +2,11 @@ import sys
 
 print(sys.version)
 print("Hello !!")
+
+# using + for concatenation.
+name = "John"
+subs = 10000
+print("Hello " + name + ", you have " + str(subs) + " subscribers.")
+
+# using f'string for concatenation.
+print(f"Hello {name}, you have {subs} subscribers.")
