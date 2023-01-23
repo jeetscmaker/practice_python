efdb963566433befeb1075c7ae16fbf7ec87ee10
Jitendra <jeetscmaker@gmail.com>
2023-01-23 10:49:12 +0530

    match statement

--- first.py
+++ first.py
@@ -10,3 +10,17 @@ print("Hello " + name + ", you have " + str(subs) + " subscribers.")
 
 # using f'string for concatenation.
 print(f"Hello {name}, you have {subs} subscribers.")
+
+# point is an (x, y) tuple
+point = -1, 2
+match point:
+    case (0, 0):
+        print("Origin")
+    case (0, y):
+        print(f"Y={y}")
+    case (x, 0):
+        print(f"X={x}")
+    case (x, y):
+        print(f"X={x}, Y={y}")
+    case _:
+        raise ValueError("Not a point")
